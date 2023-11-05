749df11e7d04057f86a342d5435e82240706f3fb
VuHo <hovanvu26699@gmail.com>
2023-11-05 14:27:26 +0700

    update basename route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/shopyame">
       <App />
     </BrowserRouter>
   </Provider>
